125b1b831a34e940e6e95020076cca171888a5b5
Clément PAQUIS <clementpaquis@gmail.com>
2018-11-07 16:28:49 +0100

    projet de base

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+    <button v-on:click="count++">Vous m'avez cliqué {{ count }} fois.</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+    data: function() {
+        return  {
+            count : 0
+        }
+    }
+}
+</script>
